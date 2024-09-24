199e8fee5ccf23afe607c66ab87a4f58e7ad1354
Léo LEESCO <leo.leesco@gmail.com>
2024-09-24 17:50:56 +0200

    peano testé

--- nat.ml
+++ nat.ml
@@ -2,11 +2,18 @@ type nat =
   | Z
   | S of nat
 
-
 let rec add x y =
   match x with
   | Z -> y
   | S x' -> S (add x' y)
+
+let () =
+  let one = S Z in
+  let two = S one in
+  let three = S two in
+  let four = S three in
+  let five = S four in
+  assert(five = add two three)
 
 let rec even x =
   match x with
@@ -27,12 +34,24 @@ let rec half x =
   | S Z -> Z
   | S S x' -> S (half x')
 
-let half_panic x =
-  halfwa Some x
+let () =
+  let one = S Z in
+  let two = S one in
+  let three = S two in
+  let four = S three in
+  let five = S four in
+  assert(two = half five)
 
 let rec halfwa x =
   match x with
   | None -> None
   | Some Z -> Some Z
-  | S Z -> None
-  | Some S S x' -> Some S (halfwa (Some x'))
+  | Some (S Z) -> None
+  | Some (S (S x')) -> (
+    match halfwa (Some x') with
+    | Some half_pred -> Some (S (half_pred))
+    | None -> None
+  )
+
+let half_panic x =
+  halfwa (Some x)
