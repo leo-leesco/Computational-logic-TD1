25fc0ee56b77ea388e39eb79c0dc1f7e4e91cd1f
Léo LEESCO <leo.leesco@gmail.com>
2025-06-24 11:48:06 -0700

    rename and format

--- nat.ml
+++ nat.ml
@@ -1,11 +1,6 @@
-type nat =
-  | Z
-  | S of nat
+type nat = Z | S of nat
 
-let rec add x y =
-  match x with
-  | Z -> y
-  | S x' -> S (add x' y)
+let rec add x y = match x with Z -> y | S x' -> S (add x' y)
 
 let () =
   let one = S Z in
@@ -13,26 +8,14 @@ let () =
   let three = S two in
   let four = S three in
   let five = S four in
-  assert(five = add two three)
+  assert (five = add two three)
 
-let rec even x =
-  match x with
-  | Z -> true
-  | S Z -> false
-  | S S x' -> even x'
+let rec even x = match x with Z -> true | S Z -> false | S (S x') -> even x'
 
-  (* ou alors faire NOT du prédécesseur *)
+(* ou alors faire NOT du prédécesseur *)
 
-let rec pred x =
-  match x with
-  | Z -> None
-  | S x' -> Some x'
-
-let rec half x =
-  match x with
-  | Z -> Z
-  | S Z -> Z
-  | S S x' -> S (half x')
+let rec pred x = match x with Z -> None | S x' -> Some x'
+let rec half x = match x with Z -> Z | S Z -> Z | S (S x') -> S (half x')
 
 let () =
   let one = S Z in
@@ -40,18 +23,17 @@ let () =
   let three = S two in
   let four = S three in
   let five = S four in
-  assert(two = half five)
+  assert (two = half five)
 
-let rec halfwa x =
+let rec halfway x =
   match x with
   | None -> None
   | Some Z -> Some Z
   | Some (S Z) -> None
   | Some (S (S x')) -> (
-    match halfwa (Some x') with
-    | Some half_pred -> Some (S (half_pred))
-    | None -> None
-  )
+      match halfway (Some x') with
+      | Some half_pred -> Some (S half_pred)
+      | None -> None)
 
-let half_panic x =
-  halfwa (Some x)
+let half_panic x = halfway (Some x)
+
